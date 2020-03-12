8fcf348acdf490a2948215470db4308d19e84b3c
571695 <571695@FEN-052-072>
2020-03-12 14:48:19 +0000

    added in threading however does not work for 1000 threads

--- MyServer/Program.cs
+++ MyServer/Program.cs
@@ -14,7 +14,8 @@ namespace MyServer
     class Program
     {
         static Dictionary<string, string> dictionary = new Dictionary<string, string>();
-  
+        
+        
 
         static void Main(string[] args)
         {
@@ -22,24 +23,26 @@ namespace MyServer
             runServer();
 
         }
+        
         static void runServer()
         {
             TcpListener listener;
             Socket connection;
-            NetworkStream socketStream;
+            Handler RequestHandler;
+            
 
             try
             {
                 listener = new TcpListener(IPAddress.Any, 43);
                 listener.Start();
+                Console.WriteLine("Server started listening");
                 while (true)
                 {
                     connection = listener.AcceptSocket();
-                    socketStream = new NetworkStream(connection);
-                   socketStream.ReadTimeout = 500;
-                    handle(socketStream);
-                    socketStream.Close();
-                    connection.Close();
+                    RequestHandler = new Handler();
+                    Thread t = new Thread(() => RequestHandler.doRequest(connection));
+                    t.Start();
+                   
                 }
             }
             catch (Exception e)
@@ -47,190 +50,198 @@ namespace MyServer
                 Console.WriteLine("Exception: " + e.ToString());
             }
         }
-        
-        static void handle (NetworkStream socketStream)
-        {
-            string line;
-            string name;
-            string location;
 
-            try
+        class Handler
+        {
+
+            public void doRequest(Socket connection)
             {
-                StreamWriter sw = new StreamWriter(socketStream);
-                StreamReader sr = new StreamReader(socketStream);
+                string line;
+                string name;
+                string location;
+
+                NetworkStream socketStream;
+                socketStream = new NetworkStream(connection);
+                Console.WriteLine("Connection Recieved");
+                //socketStream.ReadTimeout = 500;
+
                 try
                 {
-                   
+                    
+                    StreamWriter sw = new StreamWriter(socketStream);
+                    StreamReader sr = new StreamReader(socketStream);
+                    try
+                    {
+
                         line = sr.ReadLine();
-                   
+                       
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        return;
+                    }
+
+                    
+                        string[] split = line.Split(' ');
+                    
+
+                    if (line.EndsWith("HTTP/1.0"))
+                    {
+                        if (split[0] == "GET" || (split[0] == "GET" && split[1].StartsWith("/")))
+                        {
+                            name = split[1].Substring(2);
+                            if (dictionary.ContainsKey(name))
+                            {
+                                sw.WriteLine("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\nis being tested\r\n");
+                                sw.Flush();
+                            }
+                            else if (!dictionary.ContainsKey(name))
+                            {
+                                sw.WriteLine("HTTP/1.0 404 Not Found\r\nContent-Type: text/plain\r\n\r\n");
+                                sw.Flush();
+                            }
+                        }
+                        else if (split[0] == "POST")
+                        {
+                            name = split[1].Substring(1);
+                            do
+                            {
+                                line = sr.ReadLine();
+                            } while (line.Length != 0);         // want the very last line, atm is producing the optional header lines
+
+                            dictionary[name] = line;
+
+                            sw.WriteLine("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n");
+                            sw.Flush();
+                        }
+
+                    }
+                    else if (line.EndsWith("HTTP/1.1"))
+                    {
+                        if (split[0] == "GET")
+                        {
+                            name = split[1].Substring(7);
+                            if (dictionary.ContainsKey(name))
+                            {
+                                sw.WriteLine("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nis being tested");
+
+                                sw.Flush();
+                            }
+                            else
+                            {
+                                sw.WriteLine("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n");
+                                sw.Flush();
+                            }
+                        }
+                        else if (split[0] == "POST")
+                        {
+
+                            do
+                            {
+                                line = sr.ReadLine();
+                            } while (line.Contains("name=<name>&location=<>"));
+
+                            name = split[2];
+                            location = split.Last();
+                            dictionary[name] = location;
+
+                            sw.WriteLine("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n");
+                            sw.Flush();
+                        }
+                    }
+
+                    else         //0.9 or whois
+                    {     
+                        
+                        if ((split[0] == "GET" || split[0] == "PUT") && split.Length == 1)   // if the name is GET for whois, so this is for whois
+                        {
+                            name = split[0];
+                            if (dictionary.ContainsKey(name))
+                            {
+                                sw.WriteLine(dictionary[name]);
+                                sw.Flush();
+                            }
+                            else
+                            {
+                                sw.WriteLine("ERROR: no entries found");
+                                sw.Flush();
+                            }
+
+                        }
+                        else if (split[0] == "GET" && split[1].StartsWith("/"))     // server query for the name for 0.9
+                        {
+                            name = split[1].Substring(1);
+
+                            if (dictionary.ContainsKey(name))
+                            {
+                                sw.WriteLine("HTTP/0.9 200 OK\r\nContent-Type: text/plain\r\n\r\nis being tested");
+
+                                sw.Flush();
+                            }
+                            else if (!dictionary.ContainsKey(name))
+                            {
+                                sw.WriteLine("HTTP/0.9 404 Not Found\r\nContent-Type: text/plain\r\n\r\n");
+                                sw.Flush();
+                            }
+                        }
+
+
+                        else if (split[0] == "PUT" && split[1].StartsWith("/"))  // if the protocol is 0.9 and it is an update
+                                                                                  // however hits this if its a whois where the name is PUT and location starts with a /
+                        {
+                            name = split[1].Substring(1);
+                            do
+                            {
+                                line = sr.ReadLine();
+                            } while (line.Length == 1);
+                            
+                            dictionary[name] = line;
+
+                            sw.WriteLine("HTTP/0.9 200 OK\r\nContent-Type: text/plain\r\n\r\n");
+                            sw.Flush();
+                        }
+
+                        else if (split.Length == 1)         //whois          
+                        {
+                            name = split[0];
+                            if (dictionary.ContainsKey(name))
+                            {
+                                sw.WriteLine(dictionary[name]);
+                                sw.Flush();
+                            }
+                            else
+                            {
+                                sw.WriteLine("ERROR: no entries found");
+                                sw.Flush();
+                            }
+                        }
+
+                        else  //this is for the update of whois protocol
+                        {
+                            name = split[0];
+                            location = string.Join(" ", split.Skip(1).ToArray());
+                            dictionary[name] = location;
+
+                            sw.WriteLine("OK");
+                            sw.Flush();
+
+                        }
+                    }
+
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.ToString());
+                    Console.WriteLine("Something went wrong: " + e.ToString());
                     return;
                 }
-               
-                string[] split = line.Split(' ');
-               
-                if (line.EndsWith("HTTP/1.0"))
+
+                finally
                 {
-                    if (split[0] == "GET" || (split[0] == "GET" && split[1].StartsWith("/")) )
-                    {
-                        name = split[1].Substring(2);
-                        if (dictionary.ContainsKey(name))
-                        {
-                            sw.WriteLine("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\nis being tested\r\n");
-                            sw.Flush();
-                        }
-                        else if(!dictionary.ContainsKey(name))
-                        {
-                            sw.WriteLine("HTTP/1.0 404 Not Found\r\nContent-Type: text/plain\r\n\r\n");
-                            sw.Flush();
-                        }
-                    }
-                    else if (split[0] == "POST")
-                    {
-                        name = split[1].Substring(1);            
-                        do
-                        {
-                            line = sr.ReadLine();
-                        } while (line.Length != 0);         // want the very last line, atm is producing the optional header lines
-
-                        dictionary[name] = line;
-
-                        sw.WriteLine("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n");
-                        sw.Flush();
-                    }
-
+                    socketStream.Close();
+                    connection.Close();
                 }
-                else if(line.EndsWith("HTTP/1.1"))
-                {
-                    if (split[0] == "GET")
-                    {
-                        name = split[1].Substring(7);    
-                        if (dictionary.ContainsKey(name))
-                        {
-                            sw.WriteLine("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nis being tested");
-                            
-                            sw.Flush();
-                        }
-                        else
-                        {
-                            sw.WriteLine("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n");
-                            sw.Flush();
-                        }
-                    }
-                    else if (split[0] == "POST")
-                    {
-
-                        do
-                        {
-                            line = sr.ReadLine();
-                        } while (line.Contains("name=<name>&location=<>"));
-
-                        name = split[2];
-                        location = split.Last();
-                        dictionary[name] = location;
-
-                        sw.WriteLine("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n");
-                        sw.Flush();
-                    }
-                }
-
-                else         //0.9 or whois
-                {                           // TEST 30
-                    if ((split[0] == "GET" || split[0] == "PUT") && split.Length == 1)   // if the name is GET for whois, so this is for whois
-                    {
-                        name = split[0];
-                        if (dictionary.ContainsKey(name))
-                        {
-                            sw.WriteLine(dictionary[name]);
-                            sw.Flush();
-                        }
-                        else
-                        {
-                            sw.WriteLine("ERROR: no entries found");
-                            sw.Flush();
-                        }
-
-                    }
-                    else if (split[0] == "GET" && split[1].StartsWith("/"))     // server update check for 0.9
-                    {
-                        name = split[1].Substring(1);
-
-                        if (dictionary.ContainsKey(name))
-                        {
-                            sw.WriteLine("HTTP/0.9 200 OK\r\nContent-Type: text/plain\r\n\r\nis being tested");
-
-                            sw.Flush();
-                        }
-                        else if (!dictionary.ContainsKey(name))
-                        {
-                            sw.WriteLine("HTTP/0.9 404 Not Found\r\nContent-Type: text/plain\r\n\r\n");
-                            sw.Flush();
-                        }
-                    }
-                   
-                    
-                  /*  else if (split[0] == "PUT" && split[1].StartsWith("/"))  // this is for the whois protocol, when the name is PUT and the location starts with a /
-                    {                                                           // this is for further test 29 on lab 4 however cant distuniguish between this and 
-                                                                                // the PUT 0.9 update
-
-                        name = split[0];
-                        location = string.Join(" ", split.Skip(1).ToArray());
-                        dictionary[name] = location;
-
-                        sw.WriteLine("OK");
-                        sw.Flush();
-                    }*/
-                    else if (split[0] == "PUT" && split[1].StartsWith("/"))  // for the 0.9 if there is a location and it is an update
-                    {
-
-                        name = split[1].Substring(1);
-                        do
-                        {
-                            line = sr.ReadLine();
-                        } while (line.Length == 1);
-
-                        //  location = sr.ReadLine();
-                        dictionary[name] = line;
-
-                        sw.WriteLine("HTTP/0.9 200 OK\r\nContent-Type: text/plain\r\n\r\n");
-                        sw.Flush();
-                    }
-                                                                        
-                    else if (split.Length == 1)         //whois          // get the person out of the dictionary as there is no location
-                    {
-                        name = split[0];
-                        if (dictionary.ContainsKey(name))
-                        {
-                            sw.WriteLine(dictionary[name]);            
-                            sw.Flush();
-                        }
-                        else
-                        {
-                            sw.WriteLine("ERROR: no entries found");
-                            sw.Flush();
-                        }
-                    }
-
-                    else 
-                    {
-                        name = split[0];
-                        location = string.Join(" ", split.Skip(1).ToArray());
-                        dictionary[name] = location;
-
-                        sw.WriteLine("OK");
-                        sw.Flush();
-
-                    }
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                return;
             }
         }
     }
